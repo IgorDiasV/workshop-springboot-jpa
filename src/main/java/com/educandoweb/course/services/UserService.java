231dc2e464248c51504b93a7c544bdf7b6037afc
Igor Dias <igorverissimo02@hotmail.com>
2024-03-22 14:50:47 -0300

    feat: add service findbyid

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -1,12 +1,15 @@
 package com.educandoweb.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
 
+@Service
 public class UserService {
 	
 	@Autowired
@@ -14,5 +17,10 @@ public class UserService {
 
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obg = repository.findById(id);
+		return obg.get();
 	}
 }

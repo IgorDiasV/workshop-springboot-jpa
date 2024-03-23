24a3adb02794b63221cb8fe1a1f545aefdb76987
Igor Dias <igorverissimo02@hotmail.com>
2024-03-23 09:40:34 -0300

    feat: initial implement of order

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,8 @@ public class User implements Serializable{
 	private String password;
 	private String phone;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	public User() {}
 
 	public User(Long id, String name, String email, String phone, String password) {
@@ -73,6 +78,10 @@ public class User implements Serializable{
 		this.phone = phone;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -89,4 +98,5 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return id == other.id;
 	}
+
 }

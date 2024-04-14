e7099075f4fd25554704b9b1c97934a6e53b0ba9
Igor Dias <igorverissimo02@hotmail.com>
2024-04-14 09:14:55 -0300

    feat: add method to update user

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -32,4 +32,20 @@ public class UserService {
 	public void delete(long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (Exception e) {
+			throw e;
+		}	
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
